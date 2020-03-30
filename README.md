1b527b314b6f2f37cb5f795f2a26fbec5671a15d
Joni Liu <joniliu168@gmail.com>
2020-03-30 21:46:35 +0800

    + Added MongoDB for storing data. + Updated README.md.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# nodejs
-server-side rendering web portal
+### Introduction
+A server-side rendering web portal with Mongo DB.
+
+
+### Pre-requisites
+
+* A NodeJS installed. See installation guide [here](https://nodejs.org/en/download/).
+* A MongoDB Community Edition installed. See installation guide [here](https://docs.mongodb.com/manual/administration/install-community/).
+
+
+### Instructions
+
+Do `git clone` this repository, you should have `node-js` folder.
+
+> $ cd node-js
+> $ npm install
+> $ node index.js
+
+Navigate to `http://localhost:1337/` to access this web app index. But first you probably want to setup the database by accessing `http://localhost:1337/setup_db`.
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const sgfx = express();
 const port = process.env.PORT || 1337;
 
 /**
+ * DB Variables
+ */
+const MongoClient = require('mongodb').MongoClient;
+const dbURL = "mongodb://localhost:27017/"
+const dbName = "sgfxdb"
+
+/**
  *  App Configuration
  */
 sgfx.set("views", path.join(__dirname, "views"));
@@ -29,10 +36,61 @@ sgfx.get("/", (req, res) => {
 });
 
 sgfx.post("/user", (req, res) => {
-  res.render("user", {
-    title: "Profile",
-    userProfile: { nickname: "Francis Nai" }
+  // find user
+  MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+      console.error('An error occurred connecting to MongoDB: ', err);
+    } else {
+      var dbo = client.db(dbName);
+      console.log("database connected!");
+      // query users collection
+      var query = { email: "francis.nai@sgfx.com" };
+      dbo.collection("users").find(query).toArray(function(err, result) {
+        if (err) throw err;
+        // render to page
+        res.render("user", {
+          title: "Profile",
+          userProfile: result
+        });
+        // close db
+        client.close();
+      });
+    }
   });
+});
+
+sgfx.get("/setup_db", (req, res) => {
+  // create db if it's not already created
+  MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) throw err;
+    console.log("Database created!");
+    
+    let dbo = client.db(dbName);
+    // drop collection if exist
+    dbo.collection("users").drop(function(err, res) {
+      if (err) throw err;
+      if (res) console.log("Collection deleted!");
+      // create collection
+      dbo.createCollection("users", function(err, res) {
+        if (err) throw err;
+        if (res) console.log("Collection created!");
+      });
+    });
+
+    const usersObj = [
+      { userId: 1, firstName: 'Francis', lastName: 'Nai', email: 'francis.nai@sgfx.com' },
+      { userId: 2, firstName: 'Joni', lastName: 'Liu', email: 'joni.liu@sgfx.com' }
+    ];
+    // insert data into user collection
+    dbo.collection("users").insertMany(usersObj, function(err, res) {
+      if (err) throw err;
+      // close db
+      client.close();
+    });
+  });
+
+  // redirect to index page
+  res.redirect('/');
 });
 
 sgfx.get("/logout", (req, res) => {
