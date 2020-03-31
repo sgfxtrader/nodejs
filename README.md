c85eaca8763cf1fadff75ee6d4e2eb21b8a4b5b3
Joni Liu <joniliu168@gmail.com>
2020-03-31 10:09:49 +0800

    + Added IP logging into MongoDB using http://ip-api.com/json + Updated README + Added Logs page

--- README.md
+++ README.md
@@ -1,27 +1,20 @@
 ### Introduction
-A server-side rendering web portal with Mongo DB.
 
+A server-side rendering web portal with MongoDB.
 
 ### Pre-requisites
 
-* A NodeJS installed. See installation guide [here](https://nodejs.org/en/download/).
-* A MongoDB Community Edition installed. See installation guide [here](https://docs.mongodb.com/manual/administration/install-community/).
-
+- A NodeJS installed. See installation guide [here](https://nodejs.org/en/download/).
+- A MongoDB Community Edition installed. See installation guide [here](https://docs.mongodb.com/manual/administration/install-community/).
 
 ### Instructions
 
 Do `git clone` this repository, you should have `node-js` folder.
 
 > $ cd node-js
+
 > $ npm install
+
 > $ node index.js
 
-Navigate to `http://localhost:1337/` to access this web app index. But first you probably want to setup the database by accessing `http://localhost:1337/setup_db`.
-
-
-
-
-
-
-
-
+Navigate to `http://localhost:1337/` to access this web app index. But first you probably want to setup the database by accessing `http://localhost:1337/api/setup_db`.
--- index.js
+++ index.js
@@ -7,19 +7,17 @@
  */
 const express = require("express");
 const path = require("path");
+const axios = require("axios");
+// MongoDB variables
+const MongoClient = require("mongodb").MongoClient;
+const dbURL = "mongodb://localhost:27017/";
+const dbName = "sgfxdb";
 
 /**
  * App Variables
  */
 const sgfx = express();
 const port = process.env.PORT || 1337;
-
-/**
- * DB Variables
- */
-const MongoClient = require('mongodb').MongoClient;
-const dbURL = "mongodb://localhost:27017/"
-const dbName = "sgfxdb"
 
 /**
  *  App Configuration
@@ -30,69 +28,207 @@ sgfx.use(express.static(path.join(__dirname, "public")));
 
 /**
  * Routes Definitions
+ * Access index page
  */
 sgfx.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-sgfx.post("/user", (req, res) => {
+/**
+ * Access user profile page
+ */
+sgfx.get("/user", (req, res) => {
   // find user
-  MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-    if (err) {
-      console.error('An error occurred connecting to MongoDB: ', err);
-    } else {
-      var dbo = client.db(dbName);
-      console.log("database connected!");
-      // query users collection
-      var query = { email: "francis.nai@sgfx.com" };
-      dbo.collection("users").find(query).toArray(function(err, result) {
-        if (err) throw err;
-        // render to page
-        res.render("user", {
-          title: "Profile",
-          userProfile: result
-        });
-        // close db
-        client.close();
-      });
+  MongoClient.connect(
+    dbURL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      if (err) {
+        console.error("An error occurred connecting to MongoDB: ", err);
+      } else {
+        var dbo = client.db(dbName);
+        console.log("database connected!");
+        // query users collection
+        var query = { email: "francis.nai@sgfx.com" };
+        dbo
+          .collection("users")
+          .find(query)
+          .toArray(function(err, result) {
+            if (err) throw err;
+            // render to page
+            res.render("user", {
+              title: "Profile",
+              userProfile: result
+            });
+            // close db
+            client.close();
+          });
+      }
     }
-  });
+  );
 });
 
-sgfx.get("/setup_db", (req, res) => {
-  // create db if it's not already created
-  MongoClient.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-    if (err) throw err;
-    console.log("Database created!");
-    
-    let dbo = client.db(dbName);
-    // drop collection if exist
-    dbo.collection("users").drop(function(err, res) {
-      if (err) throw err;
-      if (res) console.log("Collection deleted!");
-      // create collection
-      dbo.createCollection("users", function(err, res) {
-        if (err) throw err;
-        if (res) console.log("Collection created!");
-      });
-    });
+/**
+ * Audit logs page
+ */
+sgfx.get("/logs", (req, res) => {
+  // find user
+  MongoClient.connect(
+    dbURL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      if (err) {
+        console.error("An error occurred connecting to MongoDB: ", err);
+      } else {
+        var dbo = client.db(dbName);
+        console.log("database connected!");
+        dbo
+          .collection("logs")
+          .find({})
+          .toArray(function(err, result) {
+            if (err) throw err;
+            // render to page
+            console.log(result)
+            res.render("logs", {
+              title: "Audit Logs",
+              logs: result
+            });
+            // close db
+            client.close();
+          });
+      }
+    }
+  );
+});
 
-    const usersObj = [
-      { userId: 1, firstName: 'Francis', lastName: 'Nai', email: 'francis.nai@sgfx.com' },
-      { userId: 2, firstName: 'Joni', lastName: 'Liu', email: 'joni.liu@sgfx.com' }
-    ];
-    // insert data into user collection
-    dbo.collection("users").insertMany(usersObj, function(err, res) {
-      if (err) throw err;
-      // close db
-      client.close();
+/**
+ * API for inserting log
+ */
+sgfx.post("/login", (req, res) => {
+  // TODO: validate auth here
+
+  // log user ip details
+  logIP();
+
+  // redirect to user page
+  res.redirect("/user");
+});
+
+
+/**
+ * API for inserting log
+ */
+function logIP() {
+  // get ip details
+  axios.get('http://ip-api.com/json')
+    .then(function (response) {
+      MongoClient.connect(
+        dbURL,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        (err, client) => {
+          if (err) {
+            console.error("An error occurred connecting to MongoDB: ", err);
+          } else {
+            var dbo = client.db(dbName);
+            console.log("database connected!");
+            // insert log
+            const timestamp = new Date().getTime();
+            const logObj = {
+              ipAddress: response.data.query,
+              country: response.data.country,
+              countryCode: response.data.countryCode,
+              lat: response.data.lat,
+              lon: response.data.lon,
+              isp: response.data.isp,
+              timestamp: timestamp
+            };
+            dbo.collection("logs").insertOne(logObj, function(err, res) {
+              if (err) throw err;
+              console.log("Log inserted");
+              // close db
+              client.close();
+            });
+          }
+        }
+      );
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
     });
-  });
+}
+
+/**
+ * API for setting up database
+ */
+sgfx.get("/api/setup_db", (req, res) => {
+  // create db if it's not already created
+  MongoClient.connect(
+    dbURL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      if (err) {
+        console.error("An error occurred connecting to MongoDB: ", err);
+      } else {
+        console.log("Database created!");
+
+        let dbo = client.db(dbName);
+        // drop users collection if exist
+        dbo.collection("users").drop(function(err, res) {
+          if (err) throw err;
+          if (res) console.log("User collection deleted!");
+          // create users collection
+          dbo.createCollection("users", function(err, res) {
+            if (err) throw err;
+            if (res) console.log("Users collection created!");
+          });
+        });
+
+        // drop logs collection if exist
+        dbo.collection("logs").drop(function(err, res) {
+          if (err) throw err;
+          if (res) console.log("Logs collection deleted!");
+          // create logs collection
+          dbo.createCollection("logs", function(err, res) {
+            if (err) throw err;
+            if (res) console.log("Logs collection created!");
+          });
+        });
+
+        // insert data into users collection
+        const usersObj = [
+          {
+            userId: 1,
+            firstName: "Francis",
+            lastName: "Nai",
+            email: "francis.nai@sgfx.com"
+          },
+          {
+            userId: 2,
+            firstName: "Joni",
+            lastName: "Liu",
+            email: "joni.liu@sgfx.com"
+          }
+        ];
+        dbo.collection("users").insertMany(usersObj, function(err, res) {
+          if (err) throw err;
+          // close db
+          client.close();
+        });
+      }
+    }
+  );
 
   // redirect to index page
-  res.redirect('/');
+  res.redirect("/");
 });
 
+/**
+ * Logout
+ */
 sgfx.get("/logout", (req, res) => {
   res.render("index", { title: "Home" });
 });
